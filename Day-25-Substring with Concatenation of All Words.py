368a16a1eb06d6803e10304f4dc6e7386149a303
JYOTI RANJAN SUTAR <89583677+Ranjan1231231@users.noreply.github.com>
2023-12-20 18:06:36 +0530

    Create Day-25-Substring with Concatenation of All Words.py

---
+++ Day-25-Substring with Concatenation of All Words.py
@@ -0,0 +1,69 @@
+"""
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated substring because it is not the concatenation of any permutation of words.
+Return the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+Output: [0,9]
+Explanation: Since words.length == 2 and words[i].length == 3, the concatenated substring has to be of length 6.
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+The output order does not matter. Returning [9,0] is fine too.
+Example 2:
+
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+Output: []
+Explanation: Since words.length == 4 and words[i].length == 4, the concatenated substring has to be of length 16.
+There is no substring of length 16 in s that is equal to the concatenation of any permutation of words.
+We return an empty array.
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+Output: [6,9,12]
+Explanation: Since words.length == 3 and words[i].length == 3, the concatenated substring has to be of length 9.
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"] which is a permutation of words.
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"] which is a permutation of words.
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"] which is a permutation of words.
+ 
+"""
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if not s or not words:
+            return []
+        a_word_length = len(words[0])
+        num_words = len(words)
+        total_length = a_word_length * num_words
+        word_count = dict()
+        for word in words:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+        result = list()
+        # Loop over the string in windows of size equal to total_length
+        for i in range(a_word_length):
+            left = right = i
+            current_count = dict()
+            while right + a_word_length <= len(s):
+                word = s[right: right + a_word_length]
+                right += a_word_length
+                if word in word_count:
+                    current_count[word] = current_count.get(word, 0) + 1
+                    while current_count[word] > word_count[word]:
+                        leftWord = s[left: left + a_word_length]
+                        current_count[leftWord] -= 1
+                        left += a_word_length
+                    if right - left == total_length:
+                        result.append(left)
+                else:
+                    left = right
+                    current_count.clear()
+        return result

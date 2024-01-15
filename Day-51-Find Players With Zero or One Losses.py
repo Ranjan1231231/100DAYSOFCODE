37bbec05084a4829dfd36398b77a8379cb12eab3
JYOTI RANJAN SUTAR <89583677+Ranjan1231231@users.noreply.github.com>
2024-01-15 17:07:23 +0530

    Create Day-51-Find Players With Zero or One Losses.py

---
+++ Day-51-Find Players With Zero or One Losses.py
@@ -0,0 +1,44 @@
+"""
+You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+
+Return a list answer of size 2 where:
+
+answer[0] is a list of all players that have not lost any matches.
+answer[1] is a list of all players that have lost exactly one match.
+The values in the two lists should be returned in increasing order.
+
+Note:
+
+You should only consider the players that have played at least one match.
+The testcases will be generated such that no two matches will have the same outcome.
+ 
+
+Example 1:
+
+Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+Output: [[1,2,10],[4,5,7,8]]
+Explanation:
+Players 1, 2, and 10 have not lost any matches.
+Players 4, 5, 7, and 8 each have lost one match.
+Players 3, 6, and 9 each have lost two matches.
+Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+Example 2:
+
+Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+Output: [[1,2,5,6],[]]
+Explanation:
+Players 1, 2, 5, and 6 have not lost any matches.
+Players 3 and 4 each have lost two matches.
+Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+ 
+"""
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        winners , losers = set(),[]
+        for match in matches:
+            winners.add(match[0])
+            losers.append(match[1])
+        one_losers = Counter(losers)
+        winners = winners-one_losers.keys()
+        one_losers = {k:v for k,v in one_losers.items() if v ==1 }
+        return [sorted(winners),sorted(one_losers.keys())]        

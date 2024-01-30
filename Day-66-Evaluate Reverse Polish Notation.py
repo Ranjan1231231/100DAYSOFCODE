205955346f1d197c6dc35425e15b7334d3d2b016
JYOTI RANJAN SUTAR <89583677+Ranjan1231231@users.noreply.github.com>
2024-01-30 10:14:28 +0530

    Create Day-66-Evaluate Reverse Polish Notation.py

---
+++ Day-66-Evaluate Reverse Polish Notation.py
@@ -0,0 +1,56 @@
+"""
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+ 
+"""
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token == '+':
+                stack.append(int(stack.pop() + stack.pop()))
+            elif token == '-':
+                first_tok,second_tok=stack.pop(),stack.pop()
+                stack.append(int(second_tok-first_tok))
+            elif token == '*':
+                stack.append(int(stack.pop() * stack.pop()))
+            elif token == '/':
+                first_tok,second_tok=stack.pop(),stack.pop()
+                stack.append(int(second_tok/first_tok))
+            else:
+                stack.append(int(token))
+        return stack[0]        
